ae651addfac11fd780347f0fb6489a0f79221298
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-02 12:10:12 +0600

    asyncio tqdm

---
+++ tqdm/async.py
@@ -0,0 +1,16 @@
+'''
+tqdm also supports asyncio
+'''
+
+import asyncio
+from tqdm.asyncio import tqdm
+
+
+async def main():
+    a = 0
+    async for i in tqdm(range(10000000)):
+        a += i
+    print(a)
+
+if __name__ == '__main__':
+    asyncio.run(main())
