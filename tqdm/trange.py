508112d82fe6556c2f1505473c8a632766dcdb7d
Madi <72963376+Madi-S@users.noreply.github.com>
2021-04-02 12:20:53 +0600

    tqdm trange

---
+++ tqdm/trange.py
@@ -0,0 +1,12 @@
+'''
+This example demonstrates trange usage (useful in nested loops)
+'''
+from time import sleep
+from tqdm import trange
+
+
+for i in trange(10, desc='Main Task'):
+    for j in trange(100, desc=f'Tiny Task #{i}', total=100):
+        sleep(.01)
+
+
